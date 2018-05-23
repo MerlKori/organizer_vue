d85fad2a2ae1594c269eaa1f339d42048fe66a8c
Roman <merlkori2015@gmail.com>
2018-05-23 12:33:13 +0300

    restyle calendar header

--- src/components/organizer/sidebar/CalendarHeader.vue
+++ src/components/organizer/sidebar/CalendarHeader.vue
@@ -3,9 +3,7 @@
 		<button
 			@click="prewMonth()"
 			class="header__control header__control--prew"></button>
-			<div class="header__date">
-				<span>{{isSelectDate}}</span>
-			</div>
+			<div class="header__date">{{isSelectDate}}</div>
 		<button
 				@click="nexMonth()"
 				class="header__control header__control--next"></button>
@@ -43,18 +41,22 @@ export default {
 	border-bottom: var(--border);
 	color: var(--text-color-whithe);
 }
+.header__date {
+	font-size: 1.1rem;
+	color: rgba(255,255,255, .7);
+}
 // ============ btn control =================================
 $btn-size: 25px;
-$before-size: 12px;
-$bdrs: 4px;
+$before-size: 15px;
+$bdrs: 5px;
 $margin-btn: 5%;
+$bd: 3px solid rgba(255,255,255, .6);
 
 .header__control {
 	position: relative;
 	width: $btn-size;
 	height: $btn-size;
 	background: transparent;
-	border: var(--border);
 	border-radius: $bdrs;
 	transition: background-color .25s;
 	cursor: pointer;
@@ -65,21 +67,27 @@ $margin-btn: 5%;
 		top: 25%;
 		width: $before-size;
 		height: $before-size;
-		border-top: var(--border);
-		border-left: var(--border);
+		border-top: $bd;
+		border-left: $bd;
 		border-radius: $bdrs;
 	}
-
-	&:hover {
-		background-color: rgba(255,255,255, .5);
+}
+.header__control--prew {
+	&::before {
+		transform: rotate(-45deg);
+		left: 40%;
+	}
+	&:active::before {
+		transform: rotate(-45deg) scale(.9);
 	}
 }
-.header__control--prew::before {
-	transform: rotate(-45deg);
-	left: 40%;
-}
-.header__control--next::before {
-	transform: rotate(135deg);
-	right: 30%;
+.header__control--next {
+	&::before {
+		transform: rotate(135deg);
+		right: 30%;
+	}
+	&:active::before {
+		transform: rotate(135deg) scale(.9);
+	}
 }
 </style>
